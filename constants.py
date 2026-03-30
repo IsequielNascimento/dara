13cde4d2d66600c9d0322b86995b90d258fc9148
IsequielNascimento <isequielnascimento@gmail.com>
2026-03-30 12:29:35 -0300

    update gitignore

--- constants.py
+++ constants.py
@@ -22,10 +22,10 @@ CLR_TEXT       = "#E0E0F0"   # texto padrão
 CLR_BOARD_BG   = "#C8A96E"   # fundo do tabuleiro
 CLR_BOARD_LINE = "#5C3A00"   # linhas e rótulos do tabuleiro
 
-CLR_P1         = "#1A1A2E"   # peça jogador 1 (preto-azulado)
+CLR_P1         = "#1A1A2E"   # peça jogador 1 
 CLR_P1_LT      = "#3A3A5E"   # brilho da peça 1
 CLR_P1_SHADOW  = "#3A3A3A"   # sombra da peça 1
-CLR_P2         = "#F9F9F9"   # peça jogador 2 (branco)
+CLR_P2         = "#F9F9F9"   # peça jogador 2 
 CLR_P2_DK      = "#AAAAAA"   # borda da peça 2
 CLR_P2_SHADOW  = "#999999"   # sombra da peça 2
 
